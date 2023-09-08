ab63147e510dffff4e35ac74586fc8c676e4b7cd
Ashish Kumar <ashish.b.kumar@rakuten.com>
2023-09-08 12:00:08 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// tailwind.config.js
+const { nextui } = require('@nextui-org/react');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
+};
